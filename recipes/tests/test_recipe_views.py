8832c444632e4451e96cd38408772665371323ae
Ali Marques <ali.mrques@gmail.com>
2023-12-12 19:36:21 -0300

    Testar comportamento is_published

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,7 +1,6 @@
 from django.urls import resolve, reverse
 
 from recipes import views
-from recipes.models import Recipe
 
 from .test_recipe_base import RecipeTestBase
 
@@ -34,27 +33,111 @@ class RecipeViewsTest(RecipeTestBase):
 
         # Checar se uma receita existe
         self.assertEqual(len(response.context['recipes']), 1)
+    
+    def test_recipe_home_template_dont_load_recipes_not_published(self):
+        """
+        Testar se a receita com parametro is_published=False nao e publicada
+        """
+        self.make_recipe(is_published=False)
+
+        response = self.client.get(reverse('recipes:home'))
+
+        self.assertIn(
+            'Nenhuma receita por enquanto',
+            response.content.decode('utf-8')
+        )
 
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(
             reverse('recipes:category', kwargs={'category_id':1})
         )
         self.assertIs(view.func, views.category)
+    
+    def test_recipe_category_template_dont_load_recipes_not_published(self):
+        """
+        Testar se a receita com parametro is_published=False nao e publicada
+        """
+        recipe = self.make_recipe(is_published=False)
+        response = self.client.get(
+            reverse('recipes:category', kwargs={'category_id': recipe.category.id})
+        )
+
+        self.assertEqual(response.status_code, 404)
 
     def test_recipe_category_view_returns_status_code_404_if_no_recipes_found(self):
         response = self.client.get(
-            reverse('recipes:category', kwargs={'category_id':1000})
+            reverse(
+                'recipes:category',
+                kwargs={
+                    'category_id':1000
+                }
+            )
         )
         self.assertEqual(response.status_code, 404)
     
+    def test_recipe_category_template_loads_recipes(self):
+        required_title = 'Esse é um teste da categoria'
+
+        self.make_recipe(title=required_title)
+
+        response = self.client.get(
+            reverse(
+                'recipes:category',
+                kwargs={
+                    'category_id':1,
+                }
+            )
+        )
+        content = response.content.decode('utf-8')
+
+        self.assertIn(required_title, content)
+    
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(
-            reverse('recipes:recipe', kwargs={'id':1})
+            reverse(
+                'recipes:recipe',
+                kwargs={
+                    'id':1
+                }
+            )
         )
         self.assertIs(view.func, views.recipe)
     
     def test_recipe_detail_view_returns_status_code_404_if_no_recipes_found(self):
         response = self.client.get(
-            reverse('recipes:recipe', kwargs={'id':1000})
+            reverse(
+                'recipes:recipe',
+                kwargs={
+                    'id':1000
+                }
+            )
         )
+        self.assertEqual(response.status_code, 404)
+
+    def test_recipe_detail_template_loads_the_correct_recipe(self):
+        required_title = 'Pagina da receita - Carrega uma receita'
+
+        self.make_recipe(title=required_title)
+
+        response = self.client.get(
+            reverse(
+                'recipes:recipe',
+                kwargs={
+                    'id':1
+                }
+            )
+        )
+        content = response.content.decode('utf-8')
+
+        self.assertIn(required_title, content)
+    
+    def test_recipe_detail_template_dont_load_recipe_not_published(self):
+        """
+        Testar se a receita com parametro is_published=False nao e publicada
+        """
+        recipe = self.make_recipe(is_published=False)
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': recipe.id})
+        )
+
         self.assertEqual(response.status_code, 404)
