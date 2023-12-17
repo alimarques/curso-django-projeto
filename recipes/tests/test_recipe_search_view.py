87e1a0573eebabfcd9d4dbcadc34dfc67e72c380
Ali Marques <ali.mrques@gmail.com>
2023-12-16 23:12:43 -0300

    Teste de search retornando receita com titulo

--- recipes/tests/test_recipe_search_view.py
+++ recipes/tests/test_recipe_search_view.py
@@ -26,3 +26,36 @@ class RecipeSearchViewTest(RecipeTestBase):
             'Pesquisa por &quot;&lt;teste&gt;&quot;',
             response.content.decode('utf-8')
         )
+
+    def test_recipe_search_can_find_recipe_by_title(self):
+        title1 = 'Primeira receita'
+        title2 = 'Segunda receita'
+
+        recipe1 = self.make_recipe(
+            title=title1,
+            slug='primeira-receita',
+            author_data={
+                'username':'primeiro'
+            }
+        )
+        recipe2 = self.make_recipe(
+            title=title2,
+            slug='segunda-receita',
+            author_data={
+                'username':'segunda'
+            }
+        )
+
+        search_url = reverse('recipes:search')
+        response1 = self.client.get(f'{search_url}?q={title1}')
+        response2 = self.client.get(f'{search_url}?q={title2}')
+        response_both = self.client.get(f'{search_url}?q=receita')
+
+        self.assertIn(recipe1, response1.context['recipes'])
+        self.assertNotIn(recipe2, response1.context['recipes'])
+
+        self.assertIn(recipe2, response2.context['recipes'])
+        self.assertNotIn(recipe1, response2.context['recipes'])
+        
+        self.assertIn(recipe1, response_both.context['recipes'])
+        self.assertIn(recipe2, response_both.context['recipes'])
