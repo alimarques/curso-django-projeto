12e4436ccb142432965a46c2eefa28d176830939
Ali Marques <ali.mrques@gmail.com>
2024-01-19 20:00:41 -0300

    Feat: Criar app de authors

--- project/settings.py
+++ project/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Apps do produto
     'recipes',
+    'authors',
 ]
 
 MIDDLEWARE = [
