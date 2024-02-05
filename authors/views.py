982bbae32a0fe7e4de675ba9dd9e24b381b803c9
Ali Marques <ali.mrques@gmail.com>
2024-02-05 17:51:19 -0300

    feat: Salvar forms

--- authors/views.py
+++ authors/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.http import Http404
 from django.shortcuts import redirect, render
 
@@ -18,5 +19,11 @@ def register_create(request):
     POST = request.POST
     request.session['register_form_data'] = POST
     form = RegisterForm(POST)
+
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Seu usuário foi cadastrado.')
+        
+        del(request.session['register_form_data'])
 
     return redirect('authors:register')
